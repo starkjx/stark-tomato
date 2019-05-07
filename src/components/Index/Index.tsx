54632b51710a6d975d6280aae5f798ebde690013
starkjx <303245803@qq.com>
2019-05-07 20:59:21 +0800

    注册页面完成

--- src/components/Index/Index.tsx
+++ src/components/Index/Index.tsx
@@ -12,7 +12,7 @@ class Index extends React.Component<IRouter>{
   }
 
   login(){
-    this.props.history.push('/login');
+    this.props.history.push('login');
   }
   render(){
     return (
---
+++ src/config/axios.ts
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+const appID = "yVBJrBw1HFdFq2tPdTXPgcoL"
+const appSecret = "Vkh92EDv8eQQFqChaZ5Wt7vA"
+
+const instance = axios.create({
+  baseURL: 'https://gp-server.hunger-valley.com/',
+  headers: {
+    't-app-id': appID,
+    't-app-secret': appSecret
+  }
+});
+
+// Add a request interceptor
+instance.interceptors.request.use( (config)=> {
+  const xToken = localStorage.getItem('x-token')
+  if(xToken){
+    config.headers['Authorization'] = `Bearer ${xToken}`
+  }
+  return config;
+}, (error)=> {
+  console.error(error)
+  return Promise.reject(error);
+});
+
+// Add a response interceptor
+instance.interceptors.response.use( (response)=> {
+  // Do something with response data
+  if(response.headers['x-token']){
+    localStorage.setItem('x-token',response.headers['x-token'])
+  }
+  return response;
+}, (error)=> {
+  // Do something with response error
+  return Promise.reject(error);
+});
+
+export default instance
